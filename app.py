20cab64b18d865cf048c61ab02d68f00ff6fdffa
Brian Marrs <brian.marrs@koanhealth.com>
2022-02-19 10:42:29 -0600

    Updates to debug heroku.

--- app.py
+++ app.py
@@ -367,7 +367,9 @@ def update_allied_selected_units(value, children):
     # Any differences need to be added to the UI
     ui_indexes = set(value)
 
+    print(f'Allied unit count foo: {len(allied_combat_force)}')
     unit_ids = set(map(lambda x: x.unit_id, allied_combat_force))
+    print(f'Missing IDs: {unit_ids})
 
     missing_unit_ids = ui_indexes.difference(unit_ids)
     print(f'New Unit ID: {missing_unit_ids}')
